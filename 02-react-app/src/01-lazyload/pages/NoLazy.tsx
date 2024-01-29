7b4c7e2822e183f99c0d3fba49c552472003f01d
Francisco Axel Rojas Lanuza <axlwolf@gmail.com>
2024-01-29 12:00:28 -0600

    3b. Nested routes completed

---
+++ 02-react-app/src/01-lazyload/pages/NoLazy.tsx
@@ -0,0 +1,7 @@
+export const NoLazy = () => {
+	return (
+		<div>
+			<h1>No LazyLoading component</h1>
+		</div>
+	);
+};
--- 02-react-app/src/routes/routes.ts
+++ 02-react-app/src/routes/routes.ts
@@ -1,5 +1,6 @@
 import { lazy, LazyExoticComponent } from "react";
-import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+import { NoLazy } from "../01-lazyload/pages/NoLazy";
+// import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
 
 type JSXComponent = () => JSX.Element;
 
@@ -10,36 +11,37 @@ interface Route {
 	name: string;
 }
 
-const Lazy1 = lazy(
+const LazyLayout = lazy(
 	() =>
-		import(/* webpackChunkName: "LazyPage1"*/ "../01-lazyload/pages/LazyPage1")
+		import(
+			/* webpackChunkName: "LazyLayout"*/ "../01-lazyload/layout/LazyLayout"
+		)
 );
-const Lazy2 = lazy(
-	() =>
-		import(/* webpackChunkName: "LazyPage2"*/ "../01-lazyload/pages/LazyPage2")
-);
-const Lazy3 = lazy(
-	() =>
-		import(/* webpackChunkName: "LazyPage3"*/ "../01-lazyload/pages/LazyPage3")
-);
+
+// const Lazy1 = lazy(
+// 	() =>
+// 		import(/* webpackChunkName: "LazyPage1"*/ "../01-lazyload/pages/LazyPage1")
+// );
+// const Lazy2 = lazy(
+// 	() =>
+// 		import(/* webpackChunkName: "LazyPage2"*/ "../01-lazyload/pages/LazyPage2")
+// );
+// const Lazy3 = lazy(
+// 	() =>
+// 		import(/* webpackChunkName: "LazyPage3"*/ "../01-lazyload/pages/LazyPage3")
+// );
 
 export const routes: Route[] = [
 	{
-		path: "lazy1",
-		to: "/lazy1",
-		Component: Lazy1,
-		name: "lazy1",
+		path: "/lazyload/*",
+		to: "/lazyload/",
+		Component: LazyLayout,
+		name: "Lazy Layout",
 	},
 	{
-		path: "lazy2",
-		to: "/lazy2",
-		Component: Lazy2,
-		name: "lazy2",
-	},
-	{
-		path: "lazy3",
-		to: "/lazy3",
-		Component: Lazy3,
-		name: "lazy3",
+		path: "nolazy",
+		to: "/nolazy",
+		Component: NoLazy,
+		name: "No Lazy",
 	},
 ];
